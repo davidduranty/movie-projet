708cb24f7c05b16fa0348d3ab40c114870601a31
David Duranty <dadaduranty@gmail.com>
2025-02-07 12:25:42 +0100

    feat: add utils Http

--- backend/src/controllers/movie.controller.ts
+++ backend/src/controllers/movie.controller.ts
@@ -1,12 +1,23 @@
 import { Controller, Get } from '@nestjs/common';
 import { MovieService } from '../services/movie.service';
-
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { HttpStatus } from '../utils/http-status';
 
 @Controller()
 class MovieController {
   constructor(private readonly movieService: MovieService) {}
 
- 
+  @Get('health-check')
+  @ApiOperation({
+    summary: 'Check the health of the application',
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Internal server error',
+  })
+  healthCheck() {
+    return { status: 'ready' };
+  }
 }
 
-export {MovieController}
+export { MovieController };
