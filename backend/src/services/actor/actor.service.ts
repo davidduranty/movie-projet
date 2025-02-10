0bd5b34e3b70b39bff7d61814162fcdbc81aaf78
David Duranty <dadaduranty@gmail.com>
2025-02-10 17:42:58 +0100

    feat: add method getAll actor Service

--- backend/src/services/actor/actor.service.ts
+++ backend/src/services/actor/actor.service.ts
@@ -1,6 +1,24 @@
+import { InjectRepository } from '@mikro-orm/nestjs';
 import { Injectable } from '@nestjs/common';
+import { EntityRepository, LoadStrategy, QueryOrder } from '@mikro-orm/core';
+import { Actor } from 'backend/src/entities/actor.entity';
 
 @Injectable()
-class ActorService {}
+class ActorService {
+  public constructor(
+    @InjectRepository(Actor)
+    private readonly _actorService: EntityRepository<Actor>,
+  ) {}
+
+  public async getAll(): Promise<Actor[]> {
+    const actors = await this._actorService.find(
+      {},
+      {
+        strategy: LoadStrategy.SELECT_IN,
+      },
+    );
+    return actors;
+  }
+}
 
 export { ActorService };
