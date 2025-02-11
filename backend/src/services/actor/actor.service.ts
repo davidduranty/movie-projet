26264f2591e1d1083b09299733f2d81d71458cc8
David Duranty <dadaduranty@gmail.com>
2025-02-11 12:17:47 +0100

    feat: add method post actor service and controller

--- backend/src/services/actor/actor.service.ts
+++ backend/src/services/actor/actor.service.ts
@@ -1,13 +1,20 @@
 import { InjectRepository } from '@mikro-orm/nestjs';
 import { Injectable } from '@nestjs/common';
-import { EntityRepository, LoadStrategy, QueryOrder } from '@mikro-orm/core';
+import {
+  EntityManager,
+  EntityRepository,
+  LoadStrategy,
+  QueryOrder,
+} from '@mikro-orm/core';
 import { Actor } from 'backend/src/entities/actor.entity';
+import { ActorDto } from 'backend/src/models/actor.dto';
 
 @Injectable()
 class ActorService {
   public constructor(
     @InjectRepository(Actor)
     private readonly _actorService: EntityRepository<Actor>,
+    private readonly _em: EntityManager,
   ) {}
 
   public async getAll(): Promise<Actor[]> {
@@ -33,6 +40,20 @@ class ActorService {
       },
     );
     return actors;
+  }
+
+  public async post(actorDto: ActorDto): Promise<Actor> {
+    const addActor = new Actor();
+    addActor.lastname = actorDto.lastname;
+    addActor.firstname = actorDto.firstname;
+    addActor.country = actorDto.country;
+
+    if (actorDto.start) actorDto.start = actorDto.start;
+    if (actorDto.end) actorDto.end = actorDto.end;
+
+    this._em.persistAndFlush(addActor);
+
+    return addActor;
   }
 }
 
