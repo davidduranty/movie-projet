4fca607c78067e11929f15beade0a30706138447
David Duranty <dadaduranty@gmail.com>
2025-02-14 13:35:45 +0100

    feat: add new method post in productor service and controller

--- backend/src/services/productor/productor.service.ts
+++ backend/src/services/productor/productor.service.ts
@@ -1,16 +1,23 @@
-import { EntityRepository, LoadStrategy, QueryOrder } from '@mikro-orm/core';
+import {
+  EntityManager,
+  EntityRepository,
+  LoadStrategy,
+  QueryOrder,
+} from '@mikro-orm/core';
 import { InjectRepository } from '@mikro-orm/nestjs';
 import { Injectable } from '@nestjs/common';
 import { Productor } from 'backend/src/entities/productor.entity';
+import { ProductorDto } from 'backend/src/models/productor.dto';
 
 @Injectable()
 class ProductorService {
   public constructor(
     @InjectRepository(Productor)
     private readonly _productorService: EntityRepository<Productor>,
+    private readonly _em: EntityManager,
   ) {}
 
-  public async getAll(): Promise<Productor[]> {
+  public async getAll(): Promise<ProductorDto[]> {
     const productors = await this._productorService.find(
       {},
       {
@@ -25,7 +32,7 @@ class ProductorService {
     return productors;
   }
 
-  public async get(id: number): Promise<Productor[]> {
+  public async get(id: number): Promise<ProductorDto[]> {
     const productors = await this._productorService.find(
       { id: id },
       {
@@ -38,6 +45,17 @@ class ProductorService {
       },
     );
     return productors;
+  }
+
+  public async post(productorDto: ProductorDto): Promise<ProductorDto> {
+    const addProductor = new Productor();
+    addProductor.lastname = productorDto.lastname;
+    addProductor.firstname = productorDto.firstname;
+    addProductor.age = productorDto.age;
+    addProductor.now = productorDto.now;
+    this._em.persistAndFlush(addProductor);
+
+    return addProductor;
   }
 }
 export { ProductorService };
